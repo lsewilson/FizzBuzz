d38cf733b0080a192cef729084c7f6de1c957af7
lsewilson <laurawilson@hotmail.co.uk>
2016-08-10 23:19:02 +0100

    Fizzbuzz method works

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(number)
-  return 'fizz' if number == 3
-  return 'buzz' if number == 5
-  return 'fizzbuzz' if number == 15
-  return number 
+  return 'fizzbuzz' if number % 15 == 0
+  return 'fizz' if number % 3 == 0
+  return 'buzz' if number % 5 == 0
+  return number
 end
