e74dd945474e200aac21c70f840de9fc6ee2d17d
lsewilson <laurawilson@hotmail.co.uk>
2016-08-10 23:13:11 +0100

    Wrote test for fizzbuzz(1)

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,12 @@
 require_relative '../lib/fizzbuzz'
 
 describe 'fizzbuzz' do
+  it 'returns 1 when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+end
+
+describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
