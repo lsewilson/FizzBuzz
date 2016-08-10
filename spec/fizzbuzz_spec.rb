c047f12e4757d473dc49c918ccb50cde40abf279
lsewilson <laurawilson@hotmail.co.uk>
2016-08-10 23:20:56 +0100

    added extra tests to check fizzbuzz method

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,6 +13,12 @@ describe 'fizzbuzz' do
 end
 
 describe 'fizzbuzz' do
+  it 'returns 4 when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+end
+
+describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
@@ -22,10 +28,28 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 6' do
     expect(fizzbuzz(6)).to eq 'fizz'
   end
-end 
+end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+end
 
 describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns 31 when passed 31' do
+    expect(fizzbuzz(31)).to eq 31
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed 45' do
+    expect(fizzbuzz(45)).to eq 'fizzbuzz'
   end
 end
